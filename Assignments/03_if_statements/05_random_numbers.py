1b15567c17498df6f36e514668577a61ec42fe0a
abdul565656 <abdullahkashif12b3@gmail.com>
2025-04-12 18:34:38 +0500

    if Else done

---
+++ Assignments/03_if_statements/05_random_numbers.py
@@ -0,0 +1,13 @@
+# Print 10 random numbers in the range 1 to 100.
+
+
+import random
+
+
+def main():
+ for _ in range(10):
+    print(random.randint(1, 100))
+
+
+if __name__ == "__main__":
+  main()
